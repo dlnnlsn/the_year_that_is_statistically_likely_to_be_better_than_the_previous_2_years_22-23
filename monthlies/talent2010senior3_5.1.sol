389e28f7bda84baa7dc77e6a5b47aab79c7db86b
Kgaugelo Bopape <65312478+FruityCocoButter@users.noreply.github.com>
2023-12-02 13:28:06 +0200

    Added missing March monthly diagrams

---
+++ monthlies/talent2010senior3_5.1.sol
@@ -0,0 +1,42 @@
+%!PS
+%%BoundingBox: -32 -22 32 35 
+%%HiResBoundingBox: -31.80728 -21.92043 31.58379 34.8078 
+%%Creator: MetaPost 1.005
+%%CreationDate: 2012.06.11:1735
+%%Pages: 1
+%*Font: cmmi10 9.96265 9.96265 41:e002
+%*Font: cmr10 9.96265 9.96265 31:8
+%%BeginProlog
+%%EndProlog
+%%Page: 1 1
+ 0 0 0 setrgbcolor 0 0.5 dtransform truncate idtransform setlinewidth pop
+ [] 0 setdash 1 setlinecap 1 setlinejoin 10 setmiterlimit
+newpath 0 25 moveto
+-21.6507 -12.5 lineto
+21.6507 -12.5001 lineto
+0 25 lineto
+4.33008 -5.00018 lineto
+-21.6507 -12.5 lineto stroke
+newpath 21.6507 -12.5001 moveto
+4.33008 -5.00018 lineto stroke
+-3.73601 28 moveto
+(A) cmmi10 9.96265 fshow
+ 0 3 dtransform truncate idtransform setlinewidth pop
+newpath 0 25 moveto 0 0 rlineto stroke
+23.75069 -21.4079 moveto
+(C) cmmi10 9.96265 fshow
+newpath 21.6507 -12.5001 moveto 0 0 rlineto stroke
+-31.80728 -21.40779 moveto
+(B) cmmi10 9.96265 fshow
+newpath -21.6507 -12.5 moveto 0 0 rlineto stroke
+-5.64592 -2.90019 moveto
+(O) cmmi10 9.96265 fshow
+newpath 4.33008 -5.00018 moveto 0 0 rlineto stroke
+-17.90668 8.34999 moveto
+(1) cmr10 9.96265 fshow
+-2.49068 -21.92043 moveto
+(1) cmr10 9.96265 fshow
+12.92534 8.34995 moveto
+(1) cmr10 9.96265 fshow
+showpage
+%%EOF
